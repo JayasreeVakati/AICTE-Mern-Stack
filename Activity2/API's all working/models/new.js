899083b7723a2d30d64f42eacdd0ec72aabb7e71
JayasreeVakati <VakatiJayasree93@gmail.com>
2024-12-22 14:53:56 +0530

    Update and rename new.js to Activity2/API's all working/models/new.js

---
+++ Activity2/API's all working/models/new.js
@@ -0,0 +1,28 @@
+const mongoose =require("mongoose");
+const NewsSchema=mongoose.Schema(
+    {
+        headline:{
+           type:String,
+           required: [true," please enter headline"],
+        },
+        description: {
+            type:String,
+            required:[true, " please enter Desc"],
+        },
+        link: {
+            type:String,
+            required:false,
+            default:0,
+        },
+        img: {
+            type:String,
+            required:false,
+            default:0,
+        },
+    },
+    {
+        TimeStamp:true,
+    }
+);
+const NewsModel=mongoose.model("News",NewsSchema);
+module.exports =NewsModel;
